10b0e9f0c61683713083370e075bb8a7f8530b43
NuclleaR <nuclear.890@gmail.com>
2022-08-30 12:09:56 +0100

    Initial Commit

---
+++ NuclearPlayer/NuclearPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NuclearPlayerApp.swift
+//  NuclearPlayer
+//
+//  Created by Sergey Koreniuk on 30.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct NuclearPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
